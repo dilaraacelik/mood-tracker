68c842f4a161dd8379b1656b5963fed2a3a17820
Dilara <dilara.dcelik@gmail.com>
2025-09-03 19:43:52 +0300

    remove unused imports

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,10 +1,7 @@
 'use server'
 
 import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
-
-import { createClient } from '../../../utils/supabase/server' /** burayı düzenle bacım */
-import { toast } from 'react-toastify'
+import { createClient } from '../../../utils/supabase/server' 
 
 export async function login(formData: FormData) {
 
